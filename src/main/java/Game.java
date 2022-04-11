671866f9af98c55338e553a256616ff0dd4ebc35
MajesticShekel <100454031+EthanKelly01@users.noreply.github.com>
2022-04-10 21:09:38 -0400

    Game loop added
    - Some page styling
    - Main game loop added (not fully populated)

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,20 +1,35 @@
+import javafx.animation.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Bounds;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Modality;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
-public class Game implements Runnable {
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.lang.Math.abs;
+
+public class Game {
     private main main;
-    private volatile boolean host, run = true;
+    private boolean host;
     Controller srvr = null;
     Client client = null;
+    private volatile boolean run = true;
 
     public Game(main main, int port, String address) {
         this.main = main;
@@ -31,6 +46,7 @@ public class Game implements Runnable {
     public void run() {
         Stage primaryStage = main.getStage();
 
+        AnchorPane gameScene = new AnchorPane();
         AnchorPane pPaddle = new AnchorPane();
         Stage pPaddleStage = new Stage();
 
@@ -38,7 +54,13 @@ public class Game implements Runnable {
         Stage oPaddleStage = new Stage();
 
         Rectangle2D screenBounds = Screen.getPrimary().getBounds(); //TODO: add multiscreen drifting
-        Bounds paddleBounds = ((AnchorPane)pPaddle.getScene().getRoot()).getChildren().get(0).getBoundsInLocal();
+
+        //main stage
+        Circle circle = new Circle(25, Color.valueOf("#02219e"));
+        {
+            gameScene.setStyle("-fx-background-color : #2e3034;");
+            gameScene.getChildren().addAll(circle);
+        }
 
         //Player paddle stage
         Circle paddlePong = new Circle(25, Color.valueOf("#02219e"));
@@ -51,65 +73,97 @@ public class Game implements Runnable {
             pPaddleStage.setX(screenBounds.getMaxX()*0.75); //init paddle x,y
             pPaddleStage.setY(screenBounds.getMaxY()/3);
 
-            pPaddle.getChildren().addAll(new Rectangle(70, 10, 10, 180), paddlePong);
-            pPaddleStage.setScene(new Scene(pPaddle, 100, 200));
+            pPaddle.setStyle("-fx-background-color : #2e3034;");
+
+            Rectangle paddle = new Rectangle(55, 0, 30, 206);
+            paddle.setFill(new ImagePattern(new Image("paddle.png")));
+
+            pPaddle.getChildren().addAll(paddle, paddlePong);
+            pPaddleStage.setScene(new Scene(pPaddle));
+            pPaddleStage.setWidth(100);
         }
 
         //Opponent paddle stage
         {
-
+            //do something...
         }
-
-        Circle circle = new Circle(25, Color.valueOf("#02219e"));
-        circle.setLayoutX(100); //TODO: set to mid of window
-        circle.setLayoutY(100);
-        ((AnchorPane)primaryStage.getScene().getRoot()).getChildren().addAll(circle);
-        //TODO: add paddles
-
-        double moveX = 1, moveY = 1;
-        double windowX = pPaddleStage.getX(), windowY = pPaddleStage.getY();
-
 
         //scene open
-        //show new stages, animate main stage
+        primaryStage.getScene().setRoot(gameScene);
 
-        //game loop
-        while (run) {
-            Bounds bounds = primaryStage.getScene().getRoot().getBoundsInLocal();
-            if (moveX > 5) moveX = 5;
-            if (moveY > 5) moveY = 5;
-            circle.setLayoutX(circle.getLayoutX() + moveX);
-            circle.setLayoutY(circle.getLayoutY() + moveY);
+        Thread anim = new Thread(() -> {
+            int time = 200; //milliseconds
+            long init = System.currentTimeMillis(), last = init, temp;
+            double xGrowth = (500 - primaryStage.getScene().getWidth()) / time, yGrowth = (1000 - primaryStage.getScene().getHeight()) / time;
 
-            paddlePong.setLayoutX(primaryStage.getX() + circle.getLayoutX() - pPaddleStage.getX());
-            paddlePong.setLayoutY(primaryStage.getY() + circle.getLayoutY() - pPaddleStage.getY());
-
-            //TODO: detect kicks
-            double winSpeedX = pPaddleStage.getX() - windowX, winSpeedY = pPaddleStage.getY() - windowY;
-
-            boolean alignY = paddlePong.getLayoutY() >= paddleBounds.getMinY() && paddlePong.getLayoutY() <= paddleBounds.getMaxY();
-            if (paddlePong.getLayoutX() >= paddleBounds.getMinX() - circle.getRadius() && alignY){ //hit from left
-                moveX = - (Math.abs(moveX - winSpeedX/2));
-            } else if (paddlePong.getLayoutX() <= paddleBounds.getMaxX() + circle.getRadius() && alignY){ //hit from right
-                moveX = Math.abs(moveX + winSpeedX/2);
+            while (((temp = System.currentTimeMillis()) - init) < time) {
+                long timeStep = temp - last;
+                primaryStage.setWidth(primaryStage.getWidth() + (xGrowth * timeStep));
+                primaryStage.setHeight(primaryStage.getHeight() + (yGrowth * timeStep));
+                primaryStage.setX(((screenBounds.getMaxX() - screenBounds.getMinX()) / 2) - (primaryStage.getScene().getWidth() / 2));
+                primaryStage.setY(((screenBounds.getMaxY() - screenBounds.getMinY()) / 2) - (primaryStage.getScene().getHeight() / 2));
+                last = temp;
             }
+        });
 
-            if (circle.getLayoutX() <= (bounds.getMinX() + circle.getRadius())) moveX *= -1;
+        anim.start();
 
-            //if (circle.getLayoutY() >= (screenBounds.getMaxY() - circle.getRadius()) || circle.getLayoutY() <= (screenBounds.getMinY() + circle.getRadius())) moveY *= -1;
-            if (circle.getLayoutY() >= (bounds.getMaxY() - circle.getRadius()) || circle.getLayoutY() <= (bounds.getMinY() + circle.getRadius())) moveY *= -1;
-            if (paddlePong.getLayoutX() > screenBounds.getMaxX()) {
-                circle.setLayoutX(100); //loss, restart
-                circle.setLayoutY(100);
-                moveX = moveY = 1;
+        pPaddleStage.show();
+
+        Thread temp = new Thread(() -> {
+            try {anim.join();} catch (InterruptedException ignored) {}
+
+            //loop prep
+            long last = System.currentTimeMillis();
+            double xPos = (screenBounds.getMaxX() - screenBounds.getMinX()) / 2, yPos = (screenBounds.getMaxY() - screenBounds.getMinY()) / 2;
+            double moveX = 1, moveY = 1;
+
+            //game loop
+            while (run) {
+                long time = System.currentTimeMillis();
+
+                //get info
+                {
+
+                }
+
+                //check collisions
+                {
+                    double rad = circle.getRadius();
+                    if (xPos <= primaryStage.getX() + rad) moveX = abs(moveX);
+                    else if (xPos >= primaryStage.getX() + primaryStage.getScene().getWidth() - rad) moveX = -abs(moveX);
+                    if (yPos <= primaryStage.getY() + rad) moveY = abs(moveY);
+                    else if (yPos >= primaryStage.getY() + primaryStage.getScene().getHeight() - rad) moveY = -abs(moveY);
+                }
+
+                //update positions
+                {
+                    xPos += (moveX * (time - last) / 10);
+                    yPos += (moveY * (time - last) / 10);
+
+                    circle.setLayoutX(xPos - primaryStage.getX());
+                    circle.setLayoutY(yPos - primaryStage.getY());
+
+                    paddlePong.setLayoutX(xPos - pPaddleStage.getX());
+                    paddlePong.setLayoutY(yPos - pPaddleStage.getY());
+                }
+
+                /*Bounds bounds = primaryStage.getScene().getRoot().getBoundsInLocal();
+                double winSpeedX = pPaddleStage.getX() - windowX, winSpeedY = pPaddleStage.getY() - windowY;
+                boolean alignY = paddlePong.getLayoutY() >= paddleBounds.getMinY() && paddlePong.getLayoutY() <= paddleBounds.getMaxY();
+
+                if (paddlePong.getLayoutX() >= paddleBounds.getMinX() - circle.getRadius() && alignY) { //hit from left
+                    moveX = -(Math.abs(moveX - winSpeedX / 2));
+                } else if (paddlePong.getLayoutX() <= paddleBounds.getMaxX() + circle.getRadius() && alignY) { //hit from right
+                    moveX = Math.abs(moveX + winSpeedX / 2);
+                }*/
+
+                last = time;
+                try {TimeUnit.MILLISECONDS.sleep(10);} catch (InterruptedException ignored) {}
             }
-
-            windowX = pPaddleStage.getX();
-            windowY = pPaddleStage.getY();
-        }
+        });
+        temp.start();
     }
 
-    public void end() {
-
-    }
+    public void end() { run = false; }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -19,11 +19,9 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -47,6 +45,7 @@ import java.util.regex.Pattern;
 public class main extends Application {
     private Stage primStage = null;
     public Stage getStage() { return primStage; }
+    Game game;
 
     @Override
     public void start(Stage primaryStage) throws ParserConfigurationException, IOException, SAXException {
@@ -64,6 +63,8 @@ public class main extends Application {
 
         //main menu
         {
+            home.setStyle("-fx-background-color : #2e3034;");
+
             Button hostBtn = new Button("Host Game");
             Button joinBtn = new Button("Join Game");
             hostBtn.setPrefSize(200, 10);
@@ -83,11 +84,13 @@ public class main extends Application {
             home.setAlignment(Pos.CENTER);
             home.setSpacing(10);
             home.setPadding(new Insets(20));
-            home.getChildren().addAll(hostBtn, joinBtn);
+            home.getChildren().addAll(new ImageView(new Image("logo.png")), hostBtn, joinBtn);
         }
 
         //Client menu
         {
+            clientMenu.setStyle("-fx-background-color : #f9e7bf;");
+
             clientMenu.setAlignment(Pos.CENTER);
             clientMenu.setPadding(new Insets(20));
 
@@ -124,10 +127,15 @@ public class main extends Application {
             temp3.add(new Label("Username:"), 0, 1);
             temp3.add(usernameField, 1, 1);
 
-            Button ConnectBtn = new Button("Connect");
-            portField.setOnKeyPressed(keyEvent -> {if (keyEvent.getCode() == KeyCode.ENTER) ConnectBtn.fire();});
+            Button connectBtn = new Button("Connect");
+            portField.setOnKeyPressed(keyEvent -> {if (keyEvent.getCode() == KeyCode.ENTER) connectBtn.fire();});
 
-            clientMenu.getChildren().addAll(temp2, temp, temp3, ConnectBtn);
+            connectBtn.setOnAction(e -> {
+                game = new Game(this, port, address);
+                game.run();
+            });
+
+            clientMenu.getChildren().addAll(temp2, temp, temp3, connectBtn);
         }
 
         //set stage
@@ -136,6 +144,8 @@ public class main extends Application {
             primaryStage.getIcons().add(new Image("icon.png"));
             //primaryStage.getIcons().add(new Image("icon_bg.png"));
 
+            primaryStage.setResizable(false);
+
             primaryStage.setScene(new Scene(home, 450, 300)); //v Center the window v
             primaryStage.setX(((screenBounds.getMaxX() - screenBounds.getMinX()) / 2) - (primaryStage.getScene().getWidth() / 2));
             primaryStage.setY((screenBounds.getMaxY() - screenBounds.getMinY()) / 2 - (primaryStage.getScene().getHeight() / 2));
@@ -143,6 +153,10 @@ public class main extends Application {
             //timeline
             primaryStage.show();
             //start intro anim
+
+            primaryStage.setOnCloseRequest(event -> {
+                if (game != null) game.end();
+            });
         }
     }
 
@@ -150,11 +164,3 @@ public class main extends Application {
         launch();
     }
 }
-
-/*Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
-    @Override public void handle(ActionEvent event) {
-
-    }
-}));
-timeline.setCycleCount(Animation.INDEFINITE);
-timeline.play();*/
